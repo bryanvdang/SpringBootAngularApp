3ee9979ab0b8ab102358bd6696a9eef34909f4fd
Bryan Dang <bryanvdang@gmail.com>
2019-02-01 20:31:58 -0700

    Configure API using Spring JavaConfig

--- src/main/java/com/linkedin/learning/LinkedInLearningFullStackAppAngularSpringBootApplication.java
+++ src/main/java/com/linkedin/learning/LinkedInLearningFullStackAppAngularSpringBootApplication.java
@@ -1,9 +1,13 @@
 package com.linkedin.learning;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class LinkedInLearningFullStackAppAngularSpringBootApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +15,3 @@ public class LinkedInLearningFullStackAppAngularSpringBootApplication {
 	}
 
 }
-
